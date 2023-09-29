23948b75316b3c5159c5e1770612673986bbe3d1
unknown <jul.1012@hotmail.com>
2023-09-28 21:33:25 -0500

    initialize and finished  branch project-2

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,29 @@
-const Task = ({taskName,state}) =>{
-
-    return(
-        <>
-            <input type="checkbox" value={taskName}></input>
-            <label for={taskName}>{taskName}</label>
-        </>
-
-    )
-}
+/* eslint-disable react/prop-types */
+const Task = ({ taskName, state,description, onTaskFinished }) => {
+    const handleTaskClick = () => {
+      onTaskFinished();
+      
+    };
+  
+    const taskStyle = {
+      textDecoration: state ? 'line-through' : 'none',
+    };
+  
+    return (
+      <>
+        <input
+          type="checkbox"
+          value={taskName}
+          onClick={handleTaskClick}
+          disabled={state}
+        />
+        <label htmlFor={taskName} style={taskStyle}>
+          {taskName}
+        </label>
+        <p>{description}</p>
+      </>
+    );
+  };
+  
 
 export default Task;
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,30 +1,61 @@
+import { useEffect, useState } from "react"
 import Task from "./Task"
 
 const TaskList = () =>{
 
-    const tasks = [
+
+    const [tasks,setTasks] = useState([
         {
             name:"Buy a new laptop",
-            state:false
+            state:false,
+            description : "Is a first task"
 
         },
         {
             name:"Complete a previous task",
-            state:false
+            state:false,
+            description : "Is a second task"
+
 
         },
         {
             name:"Create a video of youtube",
-            state:false
+            state:false,
+            description : "Is a  task"
 
         }
-    ]
+
+    ])
+
+
+    const taskFinished = (index) =>{
+        const newTasks = [...tasks]
+        newTasks[index].state = true;
+        setTasks(newTasks);
+        const listJson =  JSON.stringify(newTasks);
+        localStorage.setItem("tasks", listJson);
+
+        
+
+    }
+
+    useEffect(()=>{
+        const listaJSON = localStorage.getItem("tasks");
+        const miLista = JSON.parse(listaJSON);
+        if(miLista){
+            setTasks(miLista)
+        }
+
+        
+
+    },[tasks])
+
 
     return (
         <div>
-            {tasks.map((item) => (
-                <div>
-                    <Task taskName={item.name} state={item.state}></Task>
+            {tasks.map((item,index) => (
+                <div key={index}>
+                    <Task taskName={item.name} state={item.state}  description={item.description} onTaskFinished={() =>{taskFinished(index)}}></Task>
                 </div>
             )
             )}
